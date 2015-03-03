01fd655523065aa56451a583e05a2a4a044fc142
MichaelJonker <Michael Jonker (see project notes for my email address)>
2015-03-03 18:42:08 +0100

    review version

---
+++ examples/test_FormattedPrint/test_FormattedPrint.ino
@@ -0,0 +1,21 @@
+void printTest();
+void printSequenceTeaser();
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  while (!Serial) {} // wait for serial port to connect. Needed for Leonardo only
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(5000);
+  printDemo();
+  
+  //printSequenceTeaser();
+  
+  // Do not discuss  printSequence features on the Print enhancement review thread.
+  // a dedicated thread will be started for that.
+}
+
+// MJ development comment: do not forget to move Print* files if you changed them (see ./pv script)
